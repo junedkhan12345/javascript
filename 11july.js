e30b8fb73d119d1eb0881c10d2ee11beb18151cd
naved2003 <143585562+naved2003@users.noreply.github.com>
2023-08-30 12:32:37 +0530

    Add files via upload

---
+++ 11july.js
@@ -0,0 +1,40 @@
+function one(){
+    console.log("1")
+     two()
+     three()
+}
+
+function two(){
+    console.log("2")
+    
+}
+
+function three(){
+    console.log("3")
+    four()
+}
+
+function four(){
+    console.log("4")
+}
+
+one()
+
+let learnVern="Hello World!";
+console.log(typeof learnVern);
+
+
+let number=20;
+console.log(typeof number)
+
+let meena=5;
+
+if(meena <= 3){
+    console.log("right")
+}
+else{
+    console.log("false")
+}
+
+
+
