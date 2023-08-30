614821890f4c76e49c0dae1f5afe2f4bf11f0c3b
naved2003 <143585562+naved2003@users.noreply.github.com>
2023-08-30 12:33:16 +0530

    Add files via upload

---
+++ 20jun.js
@@ -0,0 +1,145 @@
+// //add two numbers 
+// function addNumber(a, b,) {
+//     console.log(a + b);
+// }
+// addNumber(10, 20);
+// //print table of a number
+
+// function table(x) {
+//     for (let i = 1; i <= 10; i++)
+//         console.log(i * x)
+
+// }
+// table(5)
+
+// //oddEven function 
+// function oddEven(number) {
+//     if (number % 2 == 0) {
+//         console.log("your number is Even")
+
+//     }
+//     else {
+//         console.log("your numbre is odd")
+//     }
+// }
+// oddEven(6)
+
+// //print Hello world N times. N is a number 
+
+// function helloPrint(numberenter) {
+//     for (let i = 1; i <= numberenter; i++)
+//         console.log("HELLO WORLD")
+
+// }
+// helloPrint(10)
+
+// //Write a function named greet that takes a name as a parameter and returns a greeting message. For example,
+// // if the input is "John," the function should return "Hello, John!".
+
+// function great(name){
+//     return ` Hello ${name}`
+//  }
+//  console.log(great("wecode academy"));
+ 
+
+// //Write a function named multiply that takes two numbers as
+// // parameters and returns their product.
+
+
+// function multiplyNumber(num1,num2){
+//     return num1 * num2
+// }
+// console.log(multiplyNumber(5,5));
+
+// // Write a function named isEven that takes a number
+// // as a parameter and returns true if the number is even,
+// // and false otherwise.
+
+// function isEven(numbervalue){
+//     if(numbervalue % 2 === 0){
+//         return true;
+//     }else {
+//         return false;
+//     }
+// }
+// console.log(isEven(14));
+
+
+// // Write a function named isOdd that takes a 
+// // number as a parameter and returns true if
+// //  the number is odd, and false otherwise.
+
+// function isOdd(number){
+//     return number % 2 != 0
+// }
+// console.log(isOdd(12));
+
+      
+// //Find max of two numbersir ye bhi question he kya
+
+// // function findMaxNumber(num1, num2) {
+// //     if (num1 > num2) {
+// //      console.log(num1)
+// //      console.log("MAX number")
+// //     } else {
+// //         console.log(num2)
+        
+// //     }
+// //   }
+// //   findMaxNumber(10,20)
+// //   findMaxNumber(9,11)
+ 
+
+
+// function cal(num1,num2,callback){
+//     callback(sum);
+// }
+// function sum(num1,num2){
+
+// }
+
+
+// Function calculate(num1, num1, cb){
+//   Cb(sum);
+// }
+// Function sum(num1, num2){
+//   Console.log(num1+num2);
+// }
+// Function sub(num1, num2){
+//   Console.log(num1-num2);
+// }
+// Function mul(num1, num2){
+//   Console.log(num1*num2);
+// }
+// Function divide(num1, num2){
+//   Console.log(num1/num2);
+// }
+// Function oddEven(num1){
+//   If(num1%2===0){
+//       Console.log("even")
+//   }Else{
+//       Console.log("odd")
+//      }
+// }
+
+
+
+
+function test(operation){
+    
+          
+}
+
+function add(num1,num2){
+    return sum = num1 + num2
+}
+function sub(num1,num2){
+    return sub1 = num1 - num2
+}
+function mul(num1,num2){
+    return mul1 = num1 * num2
+}
+function divide(num1,num2){
+        return divide = num1 / num2
+}
+add(console.log(2,2))
